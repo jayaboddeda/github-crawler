186f0f3590878e94fb955c2d7e1a4b4d258ffce7
jayaboddeda <38948963+jayaboddeda@users.noreply.github.com>
2021-06-27 19:35:49 +0530

    corrected ui

--- src/backend/controller/apicontroller.js
+++ src/backend/controller/apicontroller.js
@@ -39,10 +39,10 @@ const trending = async(req,res)=>{
 
    $('.Box-row').each((index,element)=>{
        const repotitle = $(element).find(".lh-condensed").text().replace(/\s\s+/g,"")
-       const aboutrepo = $(element).find(".pr-4").text().replace(/\s\s+/g,"")
-       const repolanguage = $(element).find(".mt-2").find('span').find("span").last().text().replace(/\s\s+/g,"").trim()
-       const repostars = $(element).find(".mt-2").find('a').first().text().replace(/\s\s+/g,"").trim()
-       const repoforks = $(element).find(".mt-2").find('a').first().next().text().replace(/\s\s+/g,"").trim()
+       const aboutrepo = $(element).find(".pr-4").text().replace(/\s\s+/g,"") || "No Info available"
+       const repolanguage = $(element).find(".mt-2").find('span').find("span").last().text().replace(/\s\s+/g,"").trim() || "No Info available"
+       const repostars = $(element).find(".mt-2").find('a').first().text().replace(/\s\s+/g,"").trim() || "No Info available"
+       const repoforks = $(element).find(".mt-2").find('a').first().next().text().replace(/\s\s+/g,"").trim() || "No Info available"
        trendingrepos[index] = {repotitle,aboutrepo,repolanguage,repostars,repoforks}
    })
    res.render("index",{
@@ -53,7 +53,12 @@ const trending = async(req,res)=>{
 
 const search = async(req,res) =>{
     let searchterm,url
+    let page
+    if(!req.query.page){
+        page = 1
+    }
     if(req.query.page && req.query.username){
+         page = req.query.page
         searchterm = req.query.username
         url = 'https://github.com/search?p='+req.query.page+'&q='+searchterm+'&type=Users'
     }
@@ -73,14 +78,15 @@ const search = async(req,res) =>{
 const searchresult = []
    $('.Box-row').each((index,element)=>{
        const userimage = $(element).find('div').first().find("a").find('img').attr('src')
-       const name = $(element).find('div').first().next().find('a.mr-1').first().text()
+       const name = $(element).find('div').first().next().find('a.mr-1').first().text() || "No Info available"
        const username =  $(element).find('div').first().next().find('a.color-text-secondary').text()
-       const userlocation =  $(element).find('div:last').text().replace(/\s\s+/g,"").trim()
+       const userlocation =  $(element).find('div:last').text().replace(/\s\s+/g,"").trim() || "No Info available"
        searchresult[index] = {userimage,name,username,userlocation}
    })
 res.render('search',{
     searchresult,
-    searchterm
+    searchterm,
+    page
 })
 }
 
@@ -95,10 +101,10 @@ const fetchuserinfo = async (req,res)=>{
 
     const uname  = $('.vcard-username').text().replace(/\s\s+/g,"").trim()
     const userimage = $('div.clearfix').find('a').first().attr('href')
-    const followers = $('.user-profile-bio').next().find('a').first().find('span').text()
-    const following = $('.user-profile-bio').next().find('a').next().find('span').first().text()
-    const stars = $('.user-profile-bio').next().find('a').next().find('span').last().text()
-    const contributions = $('.js-yearly-contributions').find('h2').first().text().replace(/\s\s+/g," ")
+    const followers = $('.user-profile-bio').next().find('a').first().find('span').text() || "No Info available"
+    const following = $('.user-profile-bio').next().find('a').next().find('span').first().text() || "No Info available"
+    const stars = $('.user-profile-bio').next().find('a').next().find('span').last().text() || "No Info available"
+    const contributions = $('.js-yearly-contributions').find('h2').first().text().replace(/\s\s+/g," ") || "No Info available"
 
     userinfo = {uname,followers,following,stars,contributions,userimage}
 
